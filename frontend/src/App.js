00473b9193db86088c4b384bc2be6e86a8538f99
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-10-05 22:53:52 +1100

    Add componenets directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import logo from './assets/logo.svg';
+import Nav from "./components/Nav";
 import './App.css';
-import lock from "./assets/lock.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <section id="profile">
-          <p>My Profile</p>
-          &nbsp;
-          <img src={lock} alt="Lock"></img>
-        </section>
-      </header>
-
+      <Nav />
       <main id="main">
         <h1>Recruit Assistant</h1>
         <h2>Find your dream job today. Without the hassle.</h2>
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import lock from "../assets/lock.svg";
+
+export default function Nav() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <section id="profile">
+          <p>My Profile</p>
+          &nbsp;
+          <img src={lock} alt="Lock"></img>
+        </section>
+      </header>
+    )
+}
