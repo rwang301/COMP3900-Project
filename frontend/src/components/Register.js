32d21a5d6f65664fc9c252f50c8057a2c878c9f9
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-10-08 04:50:22 +1100

    Add radio boxes in the registeration form

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -11,6 +11,10 @@ export default function Register(props) {
         <input type="email" placeholder="Email Address"></input>
         <input type="password" placeholder="Password"></input>
         <input type="password" placeholder="Confirm Password"></input>
+        <input type="radio" id="seeker" name="role"></input>
+        <label for="seeker">I'm looking for a job</label>
+        <input type="radio" id="employer" name="role"></input>
+        <label for="employer">I'm an Employer</label>
       </form>
       <p onClick={props.setLogin}>Already had an account? No worries, come login up here</p>
       <Button>Register</Button>
