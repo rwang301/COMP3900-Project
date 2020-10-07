18a04b806967a067f13ef4262808cfea925cfbee
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-10-07 18:18:18 +1100

    Set up database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,27 @@
 const express = require('express');
+const sqlite = require('sqlite3')
+const db = new sqlite.Database('./database.db');
+
 const app = express();
 const port = 3000;
-app.use(express.static('static'));
+
+app.get('/', () => {
+    console.log("homepage");
+    return {};
+});
+
+app.get('/users', () => {
+    console.log("users");
+    return {};
+});
+
+app.post('/user/update', (req, res) => {
+    console.log('update');
+    return {};
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+db.close();
