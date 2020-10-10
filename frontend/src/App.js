9342937be565a45119d1bcc4ef914b23ad939b58
Richard Wang <richardwang301@gmail.com>
2020-10-10 13:45:43 +1100

    forms + styledcomponents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Nav from "./components/Nav";
-import './App.css';
+import RegisterForm from "./components/RegisterForm";
+import "./App.css";
+import * as Styled from "./StyledComponents.js";
 
 function App() {
-  React.useEffect(() => {
-    fetch("http://localhost:5000").then(res => res.json()).then(data => console.log(data));
-  }, []);
+  const [showRegistration, setShowRegistration] = useState(false);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
   return (
     <div className="App">
       <Nav />
@@ -13,16 +18,27 @@ function App() {
         <h1>Recruit Assistant</h1>
         <h2>Find your dream job today. Without the hassle.</h2>
         <section id="button-container">
-          <Button>Sign Up</Button>
+          <Button onClick={() => console.log("hi shani")}>Sign Up</Button>
           <Button>Login</Button>
         </section>
+        <RegisterForm />
+        {/* {showRegistration && (
+          <div id="myModal" class="modal">
+            <div class="modal-content">
+              <span class="close">&times;</span>
+              <p>Some text in the Modal..</p>
+            </div>
+          </div>
+        )} */}
+        {showRegistration && <p>hi guys!!!!</p>}
+        <Styled.SignupButton />
       </main>
     </div>
   );
 }
 
 function Button(props) {
-  return <button className="primary-button">{props.children}</button>
+  return <button className="primary-button">{props.children}</button>;
 }
 
 export default App;
---
+++ frontend/src/StyledComponents.js
@@ -0,0 +1,13 @@
+//Buttons
+import styled from "styled-components";
+
+export const SignupButton = styled.button`
+  max-width: 20vmin;
+  width: 20vmin;
+  height: 8vmin;
+  font-size: 3vmin;
+  border-radius: 5px;
+  background: whitesmoke;
+  color: black;
+  border: 3px solid darkcyan;
+`;
