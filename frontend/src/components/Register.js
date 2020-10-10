e1b2ccedd750cf8e26744a857b47f7222adf174a
Richard Wang <richardwang301@gmail.com>
2020-10-11 01:02:27 +1100

    able to send all data for register to backend

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,18 +4,53 @@ import { Main, Header, Form, Link } from './Form';
 import Input from './Input';
 
 export default function Register(props) {
-  const register = () => {
-
+	const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+  //const [isJobseeker, setIsJobseeker] = React.useState(true);
+	const [loginstatus, setLoginstatus] = React.useState(null);
+	
+	const handleNameChange = (event) => {
+    setName(event.target.value);
   }
+	const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+	const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+	const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  }
+	// const handleJobSeekerChange = (event) => {
+  //   setName(event.target.value);
+  // }
+	async function register(e) {
+		console.log(name, email, password, confirmPassword, "KAIQU@@@@@@@@")
+		//e.preventDefault();
+		// if ()
+		const data = {name: name, email: email, password: password};
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data)
+		}
+		const response = await fetch('http://localhost:8000/auth/register', options);
+		const json = await response.json();
+		console.log(json)
+	}
 
   return (
     <Main>
       <Header>Sign Up</Header>
       <Form id="register">
-        <Input type="text" id="Name" />
-        <Input type="email" id="Email Address" />
-        <Input type="password" id="Password" />
-        <Input type="password" id="Confirm Password" />
+        <Input type="text" id="Name" value={name} handleChange={handleNameChange}/>
+        <Input type="email" id="Email Address" value={email} handleChange={handleEmailChange}/>
+        <Input type="password" id="Password" value={password} handleChange={handlePasswordChange}/>
+        <Input type="password" id="Confirm Password" value={confirmPassword} handleChange={handleConfirmPasswordChange}/>
         <input type="radio" id="seeker" name="role"></input>
         <label htmlFor="seeker">I'm looking for a job</label>
         <input type="radio" id="employer" name="role"></input>
