6d925b7e1d3b9edde2c941fd84d35bc80e8b2b9a
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-10-14 01:21:01 +1100

    Add database

---
+++ backend/db/schema.sql
@@ -0,0 +1,72 @@
+create table if not exists Users (
+    name text not null,
+    email text,
+    password text not null,
+    primary key (email)
+);
+
+create table if not exists JobSeekers (
+    email text primary key,
+    foreign key(email) references Users(email)
+);
+
+create table if not exists Employers (
+    email text primary key,
+    foreign key(email) references Users(email) 
+);
+
+
+create table if not exists Offers (
+    id serial,
+    message text not null,
+    kind text not null check (kind in ('offer', 'interview')),
+    primary key(id)
+);
+
+create table if not exists Sends (
+    employer_email text references Employers(email),
+    offer_id serial references Offers(id),
+    primary key(employer_email, offer_id)
+);
+
+
+create table if not exists Jobs (
+    id serial,
+    closing_date datetime not null,
+    description text not null,
+    responsibilities text not null,
+    remuneration float not null,
+    required_experience text not null,
+    employment_type text not null check (employment_type in ('casual', 'full-time', 'part-time')),
+    required_qualification text not null,
+    location text not null,
+    primary key(id)
+);
+
+create table if not exists Posts (
+    employer_email text references Employers(email),
+    job_id serial references Jobs(id),
+    primary key(employer_email, job_id)
+);
+
+
+create table if not exists Applications (
+    id serial,
+    cover_letter text,
+    resume text not null,
+    primary key(id)
+);
+
+create table if not exists Applies (
+    job_seeker_email text references JobSeekers(email),
+    application_id serial references Applications(id),
+    primary key(job_seeker_email, application_id)
+);
+
+
+create table if not exists Skills (
+    skill text,
+    job_seeker_email serial,
+    foreign key (job_seeker_email) references JobSeekers(email),
+    primary key (job_seeker_email, skill)
+); 
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const sqlite = require('sqlite3');
 const bodyParser = require('body-parser');
-const fs = require("fs");
 
 const app = express();
 app.use(require('cors')());
@@ -26,7 +25,21 @@ app.post('/auth/login', (req, res) => {
             if (users.length) {// if there are users in the database
                 for (const user of users) {
                     if (user.email === email && user.password === password) {
-                        sendResponse(res, 200, 'Successful login', user.employer);
+                        const query = 'select * from employers';
+                        db.all(query, [], (err, employers) => {
+                            if (err) {
+                                sendResponse(res, 500, err.message);
+                                return;
+                            } else {
+                                for (const employer of employers) {
+                                    if (employer.email === email) {
+                                        sendResponse(res, 200, 'Successful login', true);
+                                        return;
+                                    }
+                                }
+                                sendResponse(res, 200, 'Successful login', false);
+                            }
+                        });
                         return;
                     }
                 }
@@ -46,7 +59,8 @@ app.post('/auth/register', (req, res) => {
             if (user) {// if user with email already exists
                 sendResponse(res, 409, `${email} already exists`);
             } else {
-                db.run(`insert into Users values ('${name}', '${email}', '${password}', '${employer}')`);
+                db.run(`insert into Users values ('${name}', '${email}', '${password}')`);
+                db.run(`insert into ${employer ? 'Employers' : 'JobSeekers'} values ('${email}')`);
                 sendResponse(res, 200, `Inserted ${name} into the database`);
             }
         }
@@ -54,5 +68,81 @@ app.post('/auth/register', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+db.run(`
+create table if not exists Users (
+    name text not null,
+    email text,
+    password text not null,
+    primary key (email)
+)`);
 
-db.run(fs.readFileSync("./db/users.sql").toString());
+db.run(`
+create table if not exists JobSeekers (
+    email text primary key,
+    foreign key(email) references Users(email)
+)`);
+
+db.run(`
+create table if not exists Employers (
+    email text primary key,
+    foreign key(email) references Users(email)
+)`);
+
+db.run(`
+create table if not exists Offers (
+    id serial,
+    message text not null,
+    kind text not null check (kind in ('offer', 'interview')),
+    primary key(id)
+)`);
+
+db.run(`
+create table if not exists Sends (
+    employer_email text references Employers(email),
+    offer_id serial references Offers(id),
+    primary key(employer_email, offer_id)
+)`);
+
+db.run(`
+create table if not exists Jobs (
+    id serial,
+    closing_date datetime not null,
+    description text not null,
+    responsibilities text not null,
+    remuneration float not null,
+    required_experience text not null,
+    employment_type text not null check (employment_type in ('casual', 'full-time', 'part-time')),
+    required_qualification text not null,
+    location text not null,
+    primary key(id)
+)`);
+
+db.run(`
+create table if not exists Posts (
+    employer_email text references Employers(email),
+    job_id serial references Jobs(id),
+    primary key(employer_email, job_id)
+)`);
+
+db.run(`
+create table if not exists Applications (
+    id serial,
+    cover_letter text,
+    resume text not null,
+    primary key(id)
+)`)
+
+db.run(`
+create table if not exists Applies (
+    job_seeker_email text references JobSeekers(email),
+    application_id serial references Applications(id),
+    primary key(job_seeker_email, application_id)
+)`);
+
+db.run(`
+create table if not exists Skills (
+    skill text,
+    job_seeker_email serial,
+    foreign key (job_seeker_email) references JobSeekers(email),
+    primary key (job_seeker_email, skill)
+)`);
