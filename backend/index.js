5d004f2cff1407f58fda72349b83343a1a6ba74c
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-10-31 16:06:22 +1100

    Use modules

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const sqlite = require('sqlite3');
-const bodyParser = require('body-parser');
+import express from 'express';
+import sqlite from 'sqlite3';
+import bodyParser from 'body-parser';
+import createTables from './db.js';
+import cors from 'cors';
 
 const app = express();
-app.use(require('cors')());
+app.use(cors());
 app.use(bodyParser.json());
 
 const port = 8000;
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
 
 const db = new sqlite.Database('./db/database.db', err => err ? console.log(err.message) : console.log('Connected to database successfully'));
+createTables(db);
 
 const sendResponse = (response, status, message, data) => {
     response.status(status);
@@ -100,84 +103,3 @@ app.get('/matches', (req, res) => {
     });
     sendResponse(res, 200, `Getting matches for `, data);
 });
-
-
-db.run(`
-create table if not exists Users (
-    name text not null,
-    email text,
-    password text not null,
-    primary key (email)
-)`);
-
-db.run(`
-create table if not exists JobSeekers (
-    email text primary key,
-    foreign key(email) references Users(email)
-)`);
-
-db.run(`
-create table if not exists Employers (
-    email text primary key,
-    foreign key(email) references Users(email)
-)`);
-
-db.run(`
-create table if not exists Offers (
-    id integer primary key autoincrement,
-    message text not null,
-    kind text not null check (kind in ('offer', 'interview'))
-)`);
-
-db.run(`
-create table if not exists Sends (
-    employer_email text references Employers(email),
-    offer_id integer references Offers(id),
-    primary key(employer_email, offer_id)
-)`);
-
-db.run(`
-create table if not exists Jobs (
-    id integer primary key autoincrement,
-    job_title text not null,
-    location text not null,
-    description text not null,
-    employment_type text not null check (employment_type in ('casual', 'full-time', 'part-time')),
-    closing_date text not null
-)`);
-
-db.run(`
-create table if not exists Posts (
-    employer_email text references Employers(email),
-    job_id integer references Jobs(id),
-    primary key(employer_email, job_id)
-)`);
-
-db.run(`
-create table if not exists Applications (
-    id integer primary key autoincrement,
-    cover_letter text,
-    resume text not null
-)`)
-
-db.run(`
-create table if not exists Applies (
-    job_seeker_email text references JobSeekers(email),
-    application_id integer references Applications(id),
-    primary key(job_seeker_email, application_id)
-)`);
-
-db.run(`
-create table if not exists Skills (
-    skill text,
-    job_seeker_email integer,
-    foreign key (job_seeker_email) references JobSeekers(email),
-    primary key (job_seeker_email, skill)
-)`);
-
-db.run(`
-create table if not exists Matches (
-    application_id integer references Applications(id),
-    job_id integer references Jobs(id),
-    primary key (application_id, job_id)
-)`);
