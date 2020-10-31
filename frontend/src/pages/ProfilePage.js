d9ff8cb87d206709621f007db35fc4e5a2ffac4c
Richard Wang <richardwang301@gmail.com>
2020-10-31 18:59:24 +1100

    ProfilePage draft added

---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function ProfilePage() {
+
+  React.useEffect(async () => {
+    const data = {
+      "job_title":"hi",
+      "location":"lol", 
+      "description":"desc", 
+      "employment_type":"full-time", 
+      "closing_date":"202-09-19"
+    };
+    const options = {
+      body: JSON.stringify(data),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': localStorage.getItem('token')
+      },
+    };
+    const response = await fetch("http://localhost:8000/post/job", options);
+    console.log(response, "garbage!!");
+  }, []);
+
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
