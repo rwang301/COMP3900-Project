74337a4c0be8b50b2af16a8193f45a66d7f0f98c
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-11-02 03:44:13 +1100

    Linting

--- backend/src/employer.js
+++ backend/src/employer.js
@@ -3,18 +3,16 @@ import sendResponse from '../server.js';
 import { verifyToken } from './token.js';
 
 export const postJob = (req, res) => {
-    const user = verifyToken(req.header('token'))
-        .then(user => {
-            const { job_title, location, description, employment_type, closing_date } = req.body;
-            db.run(`insert into Jobs (job_title, location, description, employment_type, closing_date) values ('${job_title}', '${location}', '${description}', '${employment_type}', '${closing_date}')`);
-            db.get('select id from Jobs order by id desc', [], (err, job) => {
-                if (err) {
-                    sendResponse(res, 500, err.message);
-                } else {
-                    db.run(`insert into Posts values ('${user.email}', '${job.id}')`);
-                }
-            });
-            sendResponse(res, 200, `${user.name} posted a job`);
-        })
-        .catch(({status, message}) => sendResponse(res, status, message));
+    verifyToken(req.header('token')).then(user => {
+        const { job_title, location, description, employment_type, closing_date } = req.body;
+        db.run(`insert into Jobs (job_title, location, description, employment_type, closing_date) values ('${job_title}', '${location}', '${description}', '${employment_type}', '${closing_date}')`);
+        db.get('select id from Jobs order by id desc', [], (err, job) => {
+            if (err) {
+                sendResponse(res, 500, err.message);
+            } else {
+                db.run(`insert into Posts values ('${user.email}', '${job.id}')`);
+            }
+        });
+        sendResponse(res, 200, `${user.name} posted a job`);
+    }).catch(({status, message}) => sendResponse(res, status, message));
 };
