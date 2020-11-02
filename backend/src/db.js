dd8ec85ec96b4b2e1dc6eb49d7fa21ba6c98c9a1
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-11-03 00:58:36 +1100

    Update db according to schema

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,87 +5,85 @@ const db = new sqlite.Database('./db/database.db', err => {
     } else {
         console.log('Connected to sqlite database');
         db.run(`
-        create table if not exists Users (
-            email text,
-            name text not null,
-            password text not null,
-            token text,
-            location text,
-            primary key (email)
-        )`);
+            create table if not exists Users (
+                email text,
+                name text not null,
+                password text not null,
+                token text,
+                location text,
+                primary key (email)
+            );
+        `);
 
         db.run(`
-        create table if not exists JobSeekers (
-            email text primary key,
-            foreign key(email) references Users(email)
-        )`);
+            create table if not exists JobSeekers (
+                email text primary key,
+                foreign key(email) references Users(email)
+            );
+        `);
 
         db.run(`
-        create table if not exists Employers (
-            email text primary key,
-            company text,
-            foreign key(email) references Users(email)
-        )`);
+            create table if not exists Employers (
+                email text primary key,
+                company text,
+                foreign key(email) references Users(email)
+            );
+        `);
 
         db.run(`
-        create table if not exists Offers (
-            id integer primary key autoincrement,
-            message text not null,
-            kind text not null check (kind in ('offer', 'interview'))
-        )`);
+            create table if not exists Jobs (
+                id integer primary key autoincrement,
+                job_title text not null,
+                location text not null,
+                description text not null,
+                employment_type text not null check (employment_type in ('casual', 'full-time', 'part-time')),
+                closing_date text not null
+            );
+        `);
 
         db.run(`
-        create table if not exists Sends (
-            employer_email text references Employers(email),
-            offer_id integer references Offers(id),
-            primary key(employer_email, offer_id)
-        )`);
+            create table if not exists Posts (
+                employer_email text references Employers(email),
+                job_id integer references Jobs(id),
+                primary key(employer_email, job_id)
+            );
+        `);
 
         db.run(`
-        create table if not exists Jobs (
-            id integer primary key autoincrement,
-            job_title text not null,
-            location text not null,
-            description text not null,
-            employment_type text not null check (employment_type in ('casual', 'full-time', 'part-time')),
-            closing_date text not null
-        )`);
+            create table if not exists Skills (
+                id integer primary key autoincrement,
+                skill1 text,
+                skill2 text,
+                skill3 text,
+                job_seeker_email integer references JobSeekers(email),
+                job_id integer references Jobs(id),
+                primary key (id)
+            );
+        `);
 
         db.run(`
-        create table if not exists Posts (
-            employer_email text references Employers(email),
-            job_id integer references Jobs(id),
-            primary key(employer_email, job_id)
-        )`);
+            create table if not exists Matches (
+                job_seeker_email integer references JobSeekers(email),
+                job_id integer references Jobs(id),
+                primary key (job_seeker_email, job_id)
+            );
+        `);
 
         db.run(`
-        create table if not exists Applications (
-            id integer primary key autoincrement,
-            cover_letter text,
-            resume text not null
-        )`);
+            create table if not exists Offers (
+                id integer primary key autoincrement,
+                message text not null,
+                kind text not null check (kind in ('offer', 'interview'))
+            );
+        `);
 
         db.run(`
-        create table if not exists Applies (
-            job_seeker_email text references JobSeekers(email),
-            application_id integer references Applications(id),
-            primary key(job_seeker_email, application_id)
-        )`);
-
-        db.run(`
-        create table if not exists Skills (
-            skill text,
-            job_seeker_email integer,
-            foreign key (job_seeker_email) references JobSeekers(email),
-            primary key (job_seeker_email, skill)
-        )`);
-
-        db.run(`
-        create table if not exists Matches (
-            application_id integer references Applications(id),
-            job_id integer references Jobs(id),
-            primary key (application_id, job_id)
-        )`);
+            create table if not exists Sends (
+                employer_email text references Employers(email),
+                offer_id integer references Offers(id),
+                primary key(employer_email, offer_id)
+            );
+        `);
     }
 });
 
