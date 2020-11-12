43b9b69f5d0ac2b0594b7bb099f09c1256004ae6
Richard Wang <richardwang301@gmail.com>
2020-11-13 00:16:02 +1100

    icons added to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import styled from "styled-components";
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import instagram from '../assets/instagram.svg'
+import facebook from '../assets/facebook.svg'
+import twitter from '../assets/twitter.svg'
+import linkedin from '../assets/linkedin.svg'
+
+const SocialIcon = styled.img`
+  height: 5vh;
+  width: 5vw;
+  opacity: 40%;
+`;
+
+const IconRow = styled.div`
+  display: flex;
+  /* flex-direction: row; */
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2vh;
+`;
 
 function Copyright() {
   return (
@@ -22,6 +41,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
     backgroundColor: '#2b3d4a',
+    textAlign: 'center'
   },
 }));
 
@@ -30,7 +50,21 @@ export default function StickyFooter() {
   return (
       <footer className={classes.footer}>
         <Container maxWidth="sm">
-          <Typography variant="body1">My sticky footer can be found here.</Typography>
+          <IconRow>
+            <Link color="inherit" target='_blank' href="https://github.com/unsw-cse-capstone-project/capstone-project-comp3900-w17a-kai-will-make-tony-rich">
+              <SocialIcon src={facebook}/>
+            </Link>
+            <Link color="inherit" target='_blank' href="https://github.com/unsw-cse-capstone-project/capstone-project-comp3900-w17a-kai-will-make-tony-rich">
+              <SocialIcon src={instagram}/>
+            </Link>
+            <Link color="inherit" target='_blank' href="https://github.com/unsw-cse-capstone-project/capstone-project-comp3900-w17a-kai-will-make-tony-rich">
+              <SocialIcon src={linkedin}/>
+            </Link>
+            <Link color="inherit" target='_blank' href="https://github.com/unsw-cse-capstone-project/capstone-project-comp3900-w17a-kai-will-make-tony-rich">
+              <SocialIcon src={twitter}/>
+            </Link>
+          </IconRow>
+          <Typography variant="body1">Kaiqi Liang, William Huang, Tony Lu, Richard Wang</Typography>
           <Copyright />
         </Container>
       </footer>
