b683cf2ebf5cab7c421af87c1342a4e63e149a3d
tonylu99 <tonylu139@gmail.com>
2020-11-16 16:45:38 +1100

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -110,20 +110,35 @@ const db = new sqlite.Database('./db/database.db', (err) => {
         db.serialize(() => {
             db.run(`
                 create table if not exists SkillsList (
-                    skill text not null check (skill in ('Python', 'JavaScript', 'Java', 'C', 'C#', 'C++', 'Go', 'R', 
-                    'Swift', 'PHP', 'Dart', 'Kotlin', 'MATLAB', 'Perl')),
+                    skill text not null check (skill in ('Python', 'JavaScript', 'Java', 'C (Programming Language)', 'C#', 'C++', 'Go', 
+                        'R (Programming Language)', 'Swift', 'PHP', 'Dart', 'Kotlin', 'MATLAB', 'Perl', 'Ruby', 'Rust', 'Scala', 'Objective-C', 
+                        'TypeScript', 'Visual Basic for Applications (VBA)', 'Ada', 'Lua', 'Elm', 'Abap', 'Groovy', 'Julia', 'Cobol', 'Haskell', 'Delphi', 
+                        'SQL', 'HTML', 'CSS', 'Assembly Language', 'Git', 'Tableau', 'Microsoft Excel', 'Microsoft PowerPoint', 'Microsoft Word', 
+                        'SAS', 'SPSS', 'Microsoft Power BI', 'Hadoop', 'AutoCAD', 'Adobe Photoshop', 'Bash', 'Amazon Web Services (AWS)', 
+                        'ArcGIS', 'Frameworks', 'Google Analytics', 'Google Cloud Platform (GCP)', 'Object-Oriented Programming', 'REST API',
+                        'Rhino 3D', 'Unity', 'Windows Server', 'WordPress', 'XML', 'Machine Learning', 'Agile Methodologies', 'Adobe Dreamweaver',
+                        'Apache Maven', 'Apache Spark', 'Search Engine Optimisation (SEO)', 'Adobe Acrobat', 'Microsoft Azure', 'Microsoft Access',
+                        'Google Ads', 'Microsoft Outlook', 'Microsoft Power Automate (Flow)', 'Microsoft Project', 'Microsoft SharePoint', 'Microsoft Visio',
+                        'QuickBooks', 'Xero', 'MYOB', 'Adobe Animate', 'Adobe Illustrator', 'Adobe Lightroom', 'Adobe Premier Pro', 'Adobe XD', 'After Effects',
+                        'Autodesk Inventor', 'Autodesk Maya', 'Avid Media Composer', 'Final Cut Pro', 'iMovie', 'inDesign', 'Keynote', 'Logic Pro',
+                        'Pro Tools', 'Revit', 'SketchUp', 'SOLIDWORKS', 'Data Wrangling', 'QlikView', 'DevOps', 'Dafny', 'SOAP API', 'JSON', 'NoSQL')),
                     primary key(skill)
                 );
             `)
-                .run(`
-                    insert into SkillsList values ('Python');
-                `)
-                .run(`
-                    insert into SkillsList values ('JavaScript');
-                `)
-                .run(`
-                    insert into SkillsList values ('Java');
-                `)
+                .run('insert or replace into SkillsList values ('Python'), ('JavaScript'), ('Java'), ('C (Programming Language)'), ('C#'), ('C++'), ('Go'), 
+                    ('R (Programming Language)'), ('Swift'), ('PHP'), ('Dart'), ('Kotlin'), ('MATLAB'), ('Perl'), ('Ruby'), ('Rust'), ('Scala'), ('Objective-C'), 
+                    ('TypeScript'), ('Visual Basic for Applications (VBA)'), ('Ada'), ('Lua'), ('Elm'), ('Abap'), ('Groovy'), ('Julia'), ('Cobol'), ('Haskell'), ('Delphi'), 
+                    ('SQL'), ('HTML'), ('CSS'), ('Assembly Language'), ('Git'), ('Tableau'), ('Microsoft Excel'), ('Microsoft PowerPoint'), ('Microsoft Word'), 
+                    ('SAS'), ('SPSS'), ('Microsoft Power BI'), ('Hadoop'), ('AutoCAD'), ('Adobe Photoshop'), ('Bash'), ('Amazon Web Services (AWS)'), 
+                    ('ArcGIS'), ('Frameworks'), ('Google Analytics'), ('Google Cloud Platform (GCP)'), ('Object-Oriented Programming'), ('REST API'),
+                    ('Rhino 3D'), ('Unity'), ('Windows Server'), ('WordPress'), ('XML'), ('Machine Learning'), ('Agile Methodologies'), ('Adobe Dreamweaver'),
+                    ('Apache Maven'), ('Apache Spark'), ('Search Engine Optimisation (SEO)'), ('Adobe Acrobat'), ('Microsoft Azure'), ('Microsoft Access'),
+                    ('Google Ads'), ('Microsoft Outlook'), ('Microsoft Power Automate (Flow)'), ('Microsoft Project'), ('Microsoft SharePoint'), ('Microsoft Visio'),
+                    ('QuickBooks'), ('Xero'), ('MYOB'), ('Adobe Animate'), ('Adobe Illustrator'), ('Adobe Lightroom'), ('Adobe Premier Pro'), ('Adobe XD'), ('After Effects'),
+                    ('Autodesk Inventor'), ('Autodesk Maya'), ('Avid Media Composer'), ('Final Cut Pro'), ('iMovie'), ('inDesign'), ('Keynote'), ('Logic Pro'),
+                    ('Pro Tools'), ('Revit'), ('SketchUp'), ('SOLIDWORKS'), ('Data Wrangling'), ('QlikView'), ('DevOps'), ('Dafny'), ('SOAP API'), ('JSON'), ('NoSQL')
+                ')
+                
         });
     }
 });
